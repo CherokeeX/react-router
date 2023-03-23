c2cdec1fb8af1bbf5e6cd58a0032c7d4959d46bc
CherokeeX <serefb80@gmail.com>
2023-03-23 14:50:17 +0300

    nv links

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,15 @@
+import { NavLink } from "react-router-dom"
+
+
 export const Home = ()=>{
     return (
   
       <div className='Home'>
+
+        <nav>
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to ='/about'>About</NavLink>
+        </nav>
         <h1>DENEME ROUTER SAYFASI</h1>
         <p>TEST</p>
       </div>
